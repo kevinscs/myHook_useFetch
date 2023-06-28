5b5c2a6d9588c44cc525b4925dcf7c3cd5954f9f
kevinscs <kevinsatiro123@gmail.com>
2023-06-28 11:43:46 -0300

    first commit

---
+++ src/templates/App/index.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { useFetch } from './useFetch';
+
+
+
+const Home = () => {
+  const [postId, setPostId] = useState('');
+  const [result, loading] = useFetch('https://jsonplaceholder.typicode.com/posts/' + postId, {
+    headers: {
+      abc: '1' + postId,
+    },
+  });
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  const handleClick = (id) => {
+    setPostId(id);
+  };
+
+  if (!loading && result) {
+    // 1
+    return (
+      <div>
+        {result?.length > 0 ? (
+          result.map((p) => (
+            <div key={`post-${p.id}`} onClick={() => handleClick(p.id)}>
+              <p>{p.title}</p>
+            </div>
+          ))
+        ) : (
+          <div onClick={() => handleClick('')}>
+            <p>{result.title}</p>
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  return <h1>Oi</h1>;
+};
+
+export default Home;
